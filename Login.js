4d2fa592b762a46edb589c2d9c750c5f2bebcdde
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-05 21:10:45 +0200

    various development

--- Login.js
+++ Login.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from 'react'
 import Loader from './Loader'
+import { validateEmail } from './regex'
+import { f, auth } from './config/config'
 import {
     Text,
     View,
@@ -9,14 +11,9 @@ import {
     Dimensions,
     TouchableOpacity,
     Image,
+    ScrollView
 } from 'react-native'
-import {
-    loginWithFacebook,
-    logUserOut,
-    logUserin,
-    registerUser,
-    checkUserLogin
-} from './Auth'
+import * as Facebook from 'expo-facebook';
 
 
 
@@ -27,59 +24,128 @@ class Login extends Component {
         this.state = {
             email: '',
             password: '',
-            loading: false
+            loading: false,
+            error: ''
         }
     }
 
-    componentDidMount() {
-        console.disableYellowBox = false
+
+    // registerUser = (email, password) => {
+    //     auth.createUserWithEmailAndPassword(email, password)
+    //         .then((user) => console.log(email, password, user))
+    //         .catch((error) => console.log("Error ==> ", error))
+    //     this.setState({ loading: false })
+    // }
+
+    logUserin = async (email, password) => {
+        this.setState({ loading: true, error: '' })
+        try {
+            let user = await auth.signInWithEmailAndPassword(email, password)
+            this.props.navigation.navigate("Feed");
+            console.log(user)
+        }
+
+        catch (error) {
+            console.log(error)
+            this.setState({ error: error })
+        }
+        this.setState({ loading: false })
     }
 
-    render() {
-        const { email, password } = this.state
-        return (
-            <View style={styles.container}>
-                <Loader
-                    loading={this.state.loading} />
-                <Image
-                    style={styles.logo}
-                    source={require('./assets/insta.png')} />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor={"#ddd"}
-                    placeholder={"Email..."}
-                    value={email}
-                    onChangeText={(email) => this.setState({ email })}
-                />
+    logUserOut = () => {
+        auth.signOut().then(() => console.log("User SignedOut"))
+            .catch((error) => console.log("Error =>", error))
+        this.setState({ loading: false })
+    }
 
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor={"#ddd"}
-                    placeholder={"Password..."}
-                    value={password}
-                    onChangeText={(password) => this.setState({ password })}
-                />
-                <TouchableOpacity
-                    onPress={() => logUserin(email, password)}
-                    style={styles.button}
-                >
-                    <Text style={styles.login}>{"Login"}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={styles.forgetPassword}>
-                        {"Forgot you login Details? Get Help Signing in"}
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.fblogin}
-                    onPress={() => loginWithFacebook()}
-                    loading={this.state.loading}
-                >
-                    <Image
-                        source={require('./assets/fblogin.png')}
+    loginWithFacebook = async () => {
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+            '1048374862028026',
+            { permessions: ['email', 'public_Profile'] })
+        if (type === 'success') {
+            this.props.navigation.navigate("Feed");
+            const cred = f.auth.FacebookAuthProvider.credential(token);
+            f.auth().signInWithCredential(cred).catch((error) => console.log("Error =>", error))
+        }
+    }
+
+    checkUserLogin = () => {
+        f.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                console.log('loggedIn')
+                // this.setState({ loggedin: true })
+            }
+            else {
+                console.log('loggedOut')
+                // this.setState({ loggedin: false })
+            }
+        })
+    }
+
+
+    render() {
+        const { email, password, error } = this.state
+        const { logUserin, loginWithFacebook } = this
+        return (
+            <ScrollView
+                contentContainerStyle={styles.scroll}
+                keyboardDismissMode={'on-drag'}
+            >
+                <View style={styles.container}>
+                    <Loader
+                        loading={this.state.loading}
                     />
-                </TouchableOpacity>
-            </View >
+                    <Image
+                        style={styles.logo}
+                        source={require('./assets/insta.png')} />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor={"#ddd"}
+                        placeholder={"Email..."}
+                        value={email}
+                        onChangeText={(email) => this.setState({ email })}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor={"#ddd"}
+                        placeholder={"Password..."}
+                        value={password}
+                        onChangeText={(password) => this.setState({ password })}
+                    />
+                    {error ? <Text style={styles.error}> {error.message} </Text> : null}
+                    <TouchableOpacity
+                        onPress={() => logUserin(email, password)}
+                        style={styles.button}
+                    >
+                        <Text style={styles.login}>{"Login"}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate("ForgetPassword")}
+                    >
+                        <Text style={styles.forgetPassword}>
+                            {"Forgot you login Details? Get Help Signing in"}
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.fblogin}
+                        onPress={() => loginWithFacebook()}
+                        loading={this.state.loading}
+                    >
+                        <Image
+                            source={require('./assets/fblogin.png')}
+                        />
+                    </TouchableOpacity>
+                    <View style={styles.register}>
+                        <TouchableOpacity
+                            onPress={() => this.props.navigation.navigate("Register")}
+                        >
+                            <Text>
+                                {"Don't have account? create account now"}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View >
+            </ScrollView>
         )
     }
 }
@@ -89,9 +155,15 @@ const { width, height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#E1306C',
+    },
+    scroll: {
+        flex: 1,
+        backgroundColor: '#E1306C',
+
     },
     logo: {
         width: width * 0.85,
@@ -126,17 +198,24 @@ const styles = StyleSheet.create({
         color: '#ddd',
         fontSize: 18,
         fontWeight: '600'
-
     },
     forgetPassword: {
         color: '#ddd',
         fontSize: 12,
-        margin: 10
+        margin: 10,
     },
     fblogin: {
         width: 300,
         height: 46,
-        margin: 20
+        margin: 20,
+    },
+    error: {
+        fontSize: 12,
+        color: '#ddd'
+    },
+    register: {
+        marginTop: height * 0.3,
+        color: '#ddd'
     }
 });
 
