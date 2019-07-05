ce6ac530bef38016cab553c8b3e0b226563ab87c
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-05 04:14:16 +0200

    added Activity Indicator overlay screen component

---
+++ Loader.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    View,
+    Modal,
+    ActivityIndicator
+} from 'react-native';
+
+const Loader = props => {
+    const { loading } = props;
+    return (
+        <Modal
+            transparent={true}
+            animationType={'slide'}
+            visible={loading}
+            onRequestClose={() => { console.log('close modal') }}>
+            <View style={styles.modalBackground}>
+                <View style={styles.activityIndicatorWrapper}>
+                    <ActivityIndicator
+                        animating={loading}
+                        size={"large"}
+                    />
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    modalBackground: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        backgroundColor: '#00000040'
+    },
+    activityIndicatorWrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.22)',
+        height: '100%',
+        width: '100%',
+    }
+});
+
+export default Loader;
