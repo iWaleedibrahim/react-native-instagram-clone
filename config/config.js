4a178c1338a48584667d2ebbf7e8fa3ae0632d4d
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-05 21:12:42 +0200

    added storing object

--- config/config.js
+++ config/config.js
@@ -16,4 +16,5 @@ firebase.initializeApp(firebaseConfig);
 export const f = firebase;
 export const database = firebase.database();
 export const auth = firebase.auth();
+export const storage = firebase.storage();
 
