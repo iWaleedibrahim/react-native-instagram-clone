3862e123c734b89df774a1d2974ba9b7c6cddf70
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-05 21:10:57 +0200

    add new screen Feed

---
+++ Feed.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+class Feed extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text styles={styles.text}>
+                    This is Just a place holder text
+       </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ddd'
+    }
+});
+
+
+export default Feed;
+
