3b0b9429c4ba15296adf39a8229bfc2b82d0be00
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-05 21:14:04 +0200

    non useful code elimination

--- App.js
+++ App.js
@@ -1,32 +1,4 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
-import Login from './Login'
+import React from 'react'
+import Root from './Navigation/Root'
 
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedin: false
-    }
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Login />
-      </View>
-    );
-  }
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E1306C',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 50
-  },
-});
+export default App = () => <Root />
