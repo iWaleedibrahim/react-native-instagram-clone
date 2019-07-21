3403f976b2ed7dc21f44267215730a6c926bbff2
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-21 18:52:10 +0200

    adding custom header

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
 import { FlatList, Text, View, StyleSheet } from 'react-native'
+import { f, auth, database, storage } from '../config/config'
+
 
 class Profile extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loggedin: false
+        }
     }
+
+
+    componentDidMount() {
+        var that = this
+        f.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // loggedin
+                that.setState({ loggedin: true })
+            }
+            else {
+                // not loggedin
+                that.setState({ loggedin: false })
+            }
+        })
+    }
+
 
     render() {
         return (
-            <View>
-                <Text> {"Profile"}</Text>
+            <View style={styles.container}>
+                {this.state.loggedin == true ? (
+                    // loggedin
+                    <Text> {"IN"} </Text>
+                ) :
+                    (
+                        <View style={styles.container}>
+                            <Text> {"OUT"} </Text>
+                            <Text> {" \t You Aren't Loggedin"} </Text>
+                            <Text> {"Please login to view your profile"} </Text>
+                        </View>
+                    )}
             </View>
         )
     }
