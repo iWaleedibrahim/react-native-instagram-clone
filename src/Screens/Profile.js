da3d37be81e97084f7323398b7d8499c35bc41fc
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-22 08:01:40 +0200

    linking profile to firebase accounts

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,81 +1,127 @@
 import React, { Component } from 'react'
-import { Text, View, Image, StyleSheet, Dimensions, TouchableOpacity, FlatList } from 'react-native'
-import { f, auth, database, storage } from '../config/config'
-import Header from '../components/header'
-
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { database } from '../config/config'
 
 class Profile extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            loggedin: false
+            loaded: false,
+            name: '',
+            username: '',
+            avatar: '',
+            userId: null
         }
     }
 
-    componentDidMount() {
+    /*
+     * Some Cleanup
+     * sloidfy logic
+     */
+
+
+    checkParams = () => {
+        var params = this.props.navigation.state.params;
+        if (params.userId) {
+            this.setState({ userId: params.userId })
+        }
+        this.fetchUserInfo(params.userId)
+    }
+
+    renderNoUser = () => {
+        return (
+            <View style={styles.container}>
+                <Text> {"Please Login to View You Profile"}</Text>
+                <Text> {"Don't Have account?"}</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text> {"SignUp"}</Text>
+                    <TouchableOpacity>
+                        <Text> {"Here"}</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+
+    fetchUserInfo = (userId) => {
         var that = this
-        f.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                // loggedin
-                that.setState({ loggedin: true })
-            }
-            else {
-                // not loggedin
-                that.setState({ loggedin: false })
-            }
-        })
+        database.ref('users').child(userId).child('username').once('value')
+            .then(function (snapshot) {
+                const exists = (snapshot.val() !== null)
+                if (exists) data = snapshot.val();
+                that.setState({ username: data })
+            }).catch(e => console.log(e))
+
+        database.ref('users').child(userId).child('name').once('value')
+            .then(function (snapshot) {
+                const exists = (snapshot.val() !== null)
+                if (exists) data = snapshot.val();
+                that.setState({ name: data })
+            }).catch(e => console.log(e))
+
+        database.ref('users').child(userId).child('avatar').once('value')
+            .then(function (snapshot) {
+                const exists = (snapshot.val() !== null)
+                if (exists) data = snapshot.val();
+                that.setState({ avatar: data, loaded: true })
+            }).catch(e => console.log(e))
+    }
+
+    // componentDidMount() {
+    //     this.checkParams()
+    // }
+
+    UNSAFE_componentWillReceiveProps() {
+        this.checkParams()
     }
 
     render() {
         return (
             <View style={{ flex: 1, padding: 5 }}>
-                <Header
-                    headerText={"Profile"}
-                />
-                {this.state.loggedin == true ? (
-                    // loggedin
-                    <View style={{ flex: 1 }}>
-                        {/*
-                         * Header
-                         */}
-
-                        <View style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', paddingVertical: 10, marginTop: 20 }}>
-                            <Image
-                                source={{ uri: 'https://i.pravatar.cc/300' }}
-                                style={{ marginLeft: 10, width: 100, height: 100, borderRadius: 50 }}
-                            />
-                            <View style={{ marginRight: 10 }}>
-                                <Text>{"Name"}</Text>
-                                <Text>{"@Username"}</Text>
+                {this.state.userId == null ?
+                    (<View style={styles.container}>
+                        <Text> {"NO User Loggedin..."} </Text>
+                    </View>) : <View />}
+                {this.state.loaded == false ?
+                    (
+                        < View style={styles.container}>
+                            <Text> {"Loading..."} </Text>
+                        </View>
+                    )
+                    : (
+                        // loggedin
+                        <View style={{ flex: 1 }}>
+                            <View style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', paddingVertical: 10, marginTop: 20 }}>
+                                <Image
+                                    source={{ uri: this.state.avatar }}
+                                    style={{ marginLeft: 10, width: 100, height: 100, borderRadius: 50 }}
+                                />
+                                <View style={{ marginRight: 10 }}>
+                                    <Text>{this.state.name}</Text>
+                                    <Text>{this.state.username}</Text>
+                                </View>
+                            </View>
+                            <View>
+                                <TouchableOpacity style={{ marginTop: 10, marginHorizontal: 40, paddingVertical: 15, borderRadius: 20, borderColor: 'grey', borderWidth: 1.5 }}>
+                                    <Text style={{ color: 'grey', textAlign: 'center' }}> {'Logout'} </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={{ marginTop: 10, marginHorizontal: 40, paddingVertical: 15, borderRadius: 20, borderColor: 'grey', borderWidth: 1.5 }}>
+                                    <Text style={{ textAlign: 'center', color: 'grey' }}>{'Edit Profile'} </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    onPress={() => this.props.navigation.navigate("Upload")}
+                                    style={{ marginTop: 10, marginHorizontal: 40, paddingVertical: 25, backgroundColor: 'grey', borderRadius: 20, borderColor: 'grey', borderWidth: 1.5 }}>
+                                    <Text style={{ textAlign: 'center', color: '#fff' }}>{'Upload New +'} </Text>
+                                </TouchableOpacity>
+                            </View>
+                            {/* <View style={{ borderColor: '#555', borderWidth: 1 }} /> */}
+                            <View style={{ backgroundColor: 'green', flex: 1, justifyContent: 'center', alignItems: 'center', marginVertical: 5 }}>
+                                <Text>{"Loading Photos..."}</Text>
                             </View>
                         </View>
-                        <View>
-                            <TouchableOpacity style={{ marginTop: 10, marginHorizontal: 40, paddingVertical: 15, borderRadius: 20, borderColor: 'grey', borderWidth: 1.5 }}>
-                                <Text style={{ color: 'grey', textAlign: 'center' }}> {'Logout'} </Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ marginTop: 10, marginHorizontal: 40, paddingVertical: 15, borderRadius: 20, borderColor: 'grey', borderWidth: 1.5 }}>
-                                <Text style={{ textAlign: 'center', color: 'grey' }}>{'Edit Profile'} </Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                onPress={() => this.props.navigation.navigate("Upload")}
-                                style={{ marginTop: 10, marginHorizontal: 40, paddingVertical: 25, backgroundColor: 'grey', borderRadius: 20, borderColor: 'grey', borderWidth: 1.5 }}>
-                                <Text style={{ textAlign: 'center', color: '#fff' }}>{'Upload New +'} </Text>
-                            </TouchableOpacity>
-                        </View>
-                        {/* <View style={{ borderColor: '#555', borderWidth: 1 }} /> */}
-                        <View style={{ backgroundColor: 'green', flex: 1, justifyContent: 'center', alignItems: 'center', marginVertical: 5 }}>
-                            <Text>{"Loading Photos..."}</Text>
-                        </View>
-                    </View>
-                ) :
-                    (
-                        <View style={styles.container}>
-                            <Text> {"OUT"} </Text>
-                            <Text> {" \t You Aren't Loggedin"} </Text>
-                            <Text> {"Please login to view your profile"} </Text>
-                        </View>
-                    )}
+                    )
+                }
             </View>
         )
     }
