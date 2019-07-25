88ef02bc78ae9209449de515a9c06f7971e6a31c
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-25 16:12:07 +0200

    chaning navigator entry point

--- src/Navigation/Root.js
+++ src/Navigation/Root.js
@@ -4,13 +4,13 @@ import { createAppContainer, createStackNavigator } from 'react-navigation'
 import Auth from './Auth'
 import Home from './Home'
 
-
 const Root = createStackNavigator({
     Auth: { screen: Auth },
     Home: { screen: Home }
 }, {
-        initialRouteName: 'Home',
-        headerMode: 'none'
+        initialRouteName: 'Auth',
+        headerMode: 'none',
+        mode: 'modal',
     })
 
 export default createAppContainer(Root)
