e68ab1de604eb8e7d882b243c87991803177c633
iwaleedibrahim <iwaleedibrahim@hotmail.com>
2019-07-25 16:11:28 +0200

    minor changes

--- src/Screens/Upload.js
+++ src/Screens/Upload.js
@@ -1,59 +1,59 @@
+
 import React, { Component } from 'react'
-// import { FlatList, Text, View, StyleSheet } from 'react-native'
-// import { f, auth, database, storage } from '../config/config'
+import { Text, View, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
+import { f, auth, database, storage } from '../config/config'
 
 
-class Profile extends Component {
-    //     constructor(props) {
-    //         super(props);
-    //         this.state = {
-    //             loggedin: false
-    //         }
-    //     }
+class Upload extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedin: false
+        }
+    }
+
+    componentDidMount() {
+        var that = this
+        f.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // loggedin
+                that.setState({ loggedin: true })
+            }
+            else {
+                // not loggedin
+                that.setState({ loggedin: false })
+            }
+        })
+    }
 
 
-    //     // componentDidMount() {
-    //     //     var that = this
-    //     //     f.auth().onAuthStateChanged(function (user) {
-    //     //         if (user) {
-    //     //             // loggedin
-    //     //             this.setState({ loggedin: true })
-    //     //         }
-    //     //         else {
-    //     //             // not loggedin
-    //     //             this.setState({ loggedin: false })
-    //     //         }
-    //     //     }).then((r) => console.log(r)).catch(e => console.log(e))
-    //     // }
-
-
-    //     render() {
-    //         return (
-    //             <View>
-    //                 {this.state.loggedin == true ? (
-    //                     // loggedin
-    //                     <Text> {"Upload"} </Text>
-    //                 ) :
-    //                     (
-    //                         <View style={styles.container}>
-    //                             <Text> {"OUT"} </Text>
-    //                             <Text> {" \t You Aren't Loggedin"} </Text>
-    //                             <Text> {"Please upload your photo"} </Text>
-
-    //                         </View>
-    //                     )}
-    //             </View>
-    //         )
-    //     }
-    // }
-
-    // const styles = StyleSheet.create({
-    //     container: {
-    //         flex: 1,
-    //         justifyContent: 'center',
-    //         alignItems: 'center',
-    //         backgroundColor: '#ddd'
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.state.loggedin == true ? (
+                    // loggedin
+                    <Text> {"Upload"} </Text>
+                ) :
+                    (
+                        <View style={styles.container}>
+                            <Text> {"OUT"} </Text>
+                            <Text> {" \t You Aren't Loggedin"} </Text>
+                            <Text> {"Please login to upload a photo"} </Text>
+                        </View>
+                    )}
+            </View>
+        )
+    }
 }
-// });
 
-export { Profile };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ddd'
+    }
+});
+
+
+export { Upload };
